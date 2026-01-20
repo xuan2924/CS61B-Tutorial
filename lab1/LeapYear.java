75447b2e7762d108364b232b6a245d15d1328b0e
xuan <xuan12138@163.com>
2026-01-20 16:06:04 +0800

    完成lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,17 @@
  */
 public class LeapYear {
 
+    public static boolean isLeapYear(int year){
+        if(year%400==0){
+            return true;
+        }
+        else if(year%4==0&&year%100!=0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
@@ -13,6 +24,7 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
+
 
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
