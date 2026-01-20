f62b4706b8ec4dfbe9f7860853407503a27271f3
xuan <xuan12138@163.com>
2026-01-20 17:29:58 +0800

    完成链表初始化

---
+++ codeByMySelf/IntList.java
@@ -0,0 +1,43 @@
+public class IntList{
+    public int first;
+    public IntList rest;//next
+
+    public IntList(int f,IntList r){
+        first=f;
+        rest=r;
+    }
+
+    public int size(){
+        if(rest==null){
+            return 1;
+        }
+        else {
+            return 1 + this.rest.size();
+        }
+    }
+
+    public int iterativeSize(){
+        IntList p = this;
+        int totalSize = 0;
+        while(p != null){
+            totalSize += 1 ;
+            p = p.rest;
+        }
+        return totalSize;
+    }
+
+    public int get(int i){
+        if(i==0){
+            return first;
+        }
+        return rest.get(i-1);
+    }
+
+    public static void main(String [] args){
+        IntList L =new IntList(5,null);
+        L.rest=new IntList(10,null);
+        L.rest.rest = new IntList(15,null);
+
+        System.out.println(L.get(1));
+    }
+}
