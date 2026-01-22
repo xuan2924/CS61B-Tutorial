cf236e903d6aecd8145bc1223853a0ec9bb95a5d
xuan <xuan12138@163.com>
2026-01-22 21:52:45 +0800

    finished lab2

--- lab2/DebugPractice/DebugExercise2.java
+++ lab2/DebugPractice/DebugExercise2.java
@@ -7,12 +7,13 @@ public class DebugExercise2 {
       * This function may have a bug, but if it does, you should find it
       * by stepping over, not into. */
     public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
-
-        int max = b & w | a & z;
+//        int w = (b - a) >> 31;
+//        /* If you're stepping into this function, click the
+//           step out button because you're not going to learn anything. */
+//        int z = ~(b - a) >> 31;
+//
+//        int max = b & w | a & z;
+        int max=(a>b)?a:b;
         return max;
     }
 
@@ -48,7 +49,7 @@ public class DebugExercise2 {
         }
         int[] returnArray = new int[a.length];
         for (int i = 0; i < a.length; i += 1) {
-            int biggerValue = max(a[i], b[i]);
+            int biggerValue = max(a[i],b[i]);
             returnArray[i] = biggerValue;
         }
 
@@ -60,7 +61,7 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            sum = sum + x[i];
             i = i + 1;
         }
         return sum;
