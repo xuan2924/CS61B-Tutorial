43e5acd3e5f11afe4167078dfc1da08e4d70c686
xuan <xuan12138@163.com>
2026-01-24 23:33:25 +0800

    Dqueue

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,3 +1,33 @@
-public class ArrayDeque {
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size ;
 
+    public ArrayDeque(){
+        items = (T[]) new Object[8];
+        size = 0;
+    }
+
+    public void addFirst(T item){
+
+    }
+
+    public void addLast(T item){
+
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return this.size();
+    }
+
+    public void printDeque(){
+
+    }
+
+    public T removeFirst(){
+
+    }
 }
