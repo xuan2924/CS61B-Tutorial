7f57608c863e369c70ea06f1a5ce5e3f4b7b84af
xuan <xuan12138@163.com>
2026-01-26 15:22:54 +0800

    期望值和实际值错了

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -12,23 +12,29 @@ public class TestArrayDequeGold {
         ArrayDequeSolution<Integer> ad1 = new ArrayDequeSolution<>();
 
         String log = ""; //记录操作过程
-        for (int i = 0; i < 100; i += 1) {
+        for (int i = 0; i < 2000; i += 1) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
 
             if (numberBetweenZeroAndOne < 0.25) {
                 sad1.addFirst(i);
                 ad1.addFirst(i);
                 log += "addFirst(" + i + ")\n";
-            } else if (numberBetweenZeroAndOne >= 0.25 && numberBetweenZeroAndOne < 0.5) {
+            } else if (numberBetweenZeroAndOne < 0.5) {
                 sad1.addLast(i);
                 ad1.addLast(i);
                 log += "addLast(" + i + ")\n";
-            } else if (numberBetweenZeroAndOne >= 0.5 && numberBetweenZeroAndOne <= 0.75) {
+            } else if (numberBetweenZeroAndOne < 0.75) {
+                if(ad1.isEmpty()){
+                    continue;
+                }
                 log += "removeFirst()\n";
-                assertEquals(log, sad1.removeFirst(), ad1.removeFirst());
+                assertEquals(log, ad1.removeFirst(), sad1.removeFirst());
             } else {
+                if(ad1.isEmpty()){
+                    continue;
+                }
                 log += "removeLast()\n";
-                assertEquals(log, sad1.removeLast(), ad1.removeLast());
+                assertEquals(log, ad1.removeLast(), sad1.removeLast());
             }
         }
     }
