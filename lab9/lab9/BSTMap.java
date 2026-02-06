2760184f7e98cf3d140ee52c2a0875772aee10df
xuan <xuan12138@163.com>
2026-02-07 00:33:14 +0800

    插入删除基本实现，迭代还没有，写的还是挺顺利的

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -1,12 +1,11 @@
 package lab9;
 
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Implementation of interface Map61B with BST as core data structure.
  *
- * @author Your name here
+ * @author someone
  */
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
@@ -27,6 +26,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
     private Node root;  /* Root node of the tree. */
     private int size; /* The number of key-value pairs in the tree */
+    private V removeValue; //记录删去节点的值，避免get再循环一次
 
     /* Creates an empty BSTMap. */
     public BSTMap() {
@@ -40,66 +40,159 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         size = 0;
     }
 
-    /** Returns the value mapped to by KEY in the subtree rooted in P.
-     *  or null if this map contains no mapping for the key.
+    /**
+     * Returns the value mapped to by KEY in the subtree rooted in P.
+     * or null if this map contains no mapping for the key.
      */
     private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            throw new NoSuchElementException();
+        }
+        if (p == null) {
+            return null;
+        }
+        int cmp = key.compareTo(p.key);
+        if (cmp > 0) {
+            return getHelper(key, p.right);
+        } else if (cmp < 0) {
+            return getHelper(key, p.left);
+        } else {
+            return p.value;
+        }
     }
 
-    /** Returns the value to which the specified key is mapped, or null if this
-     *  map contains no mapping for the key.
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return getHelper(key, root);
     }
 
-    /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
-      * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
+    /**
+     * Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
+     * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
      */
     private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            return new Node(key, value);
+        }
+        int cmp = key.compareTo(p.key);
+        if (cmp > 0) {
+            p.right = putHelper(key, value, p.right);
+        } else if (cmp < 0) {
+            p.left = putHelper(key, value, p.left);
+        } else {
+            p.value = value;
+        }
+        return p;
     }
 
-    /** Inserts the key KEY
-     *  If it is already present, updates value to be VALUE.
+    /**
+     * Inserts the key KEY
+     * If it is already present, updates value to be VALUE.
      */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        root = putHelper(key, value, root);
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return sizeHelper(root);
     }
 
-    //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
+    private int sizeHelper(Node p) {
+        if (p == null) {
+            return 0;
+        }
+        return 1 + sizeHelper(p.left) + sizeHelper(p.right);
+    }
+
+    /// ///////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
 
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> set = new HashSet<>();
+        keySetHelper(root, set);
+        return set;
     }
 
-    /** Removes KEY from the tree if present
-     *  returns VALUE removed,
-     *  null on failed removal.
+    private void keySetHelper(Node p, Set<K> set) {
+        if (p == null) {
+            return;
+        }
+        keySetHelper(p.left, set);
+        set.add(p.key);
+        keySetHelper(p.right, set);
+    }
+
+    /**
+     * Removes KEY from the tree if present
+     * returns VALUE removed,
+     * null on failed removal.
      */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        removeValue = null; // 使用之前记得清空
+        root = removeHelper(root, key) ;
+        return removeValue ;
     }
 
-    /** Removes the key-value entry for the specified key only if it is
-     *  currently mapped to the specified value.  Returns the VALUE removed,
-     *  null on failed removal.
+    /**
+     * Removes the key-value entry for the specified key only if it is
+     * currently mapped to the specified value.  Returns the VALUE removed,
+     * null on failed removal.
      **/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        V res = getHelper(key,root);
+        if(res != null && res.equals(value)){
+            root = removeHelper(root,key);
+            return res;
+        } else {
+            return null;
+        }
+    }
+
+    private Node removeHelper(Node p, K key) {
+        if (p == null) {
+            return null;
+        }
+        int cmp = key.compareTo(p.key);
+        if (cmp > 0) {
+            p.right = removeHelper(p.right, key);
+        } else if (cmp < 0) {
+            p.left = removeHelper(p.left, key);
+        } else {
+            removeValue = p.value;
+
+            if (p.left == null) {
+                return p.right;
+            }
+            if (p.right == null) {
+                return p.left;
+            }
+
+            Node temp = findMin(p.right);
+            p.key = temp.key;
+            p.value = temp.value;
+            p.right = removeHelper(p.right, temp.key);
+        }
+        return p;
+    }
+
+    private Node findMin(Node p) {
+        if (p == null) {
+            return null;
+        }
+        while (p.left != null) {
+            p = p.left;
+        }
+        return p;
     }
 
     @Override
