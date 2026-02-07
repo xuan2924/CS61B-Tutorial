dacba364322a9bb5ab8b2cdb3d0591b8f52e81d0
xuan <xuan12138@163.com>
2026-02-07 22:55:54 +0800

    并查集的使用

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,104 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    //(0,0)表示左下角
 
+    private Site[][] array;
+    private int openSize;
+    private int virtualHead;
+    private int virtualTail;
+    private int N;
+    private WeightedQuickUnionUF ufForPercolation;
+    private WeightedQuickUnionUF ufForFullness;
+
+    private class Site {
+        boolean isOpen;
+        int index;
+
+        public Site(int i) {
+            isOpen = false;
+            index = i;
+        }
+    }
+
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        this.N = N;
+        array = new Site[N][N];
+        openSize = 0;
+        virtualHead = 0;
+        virtualTail = N * N + 1;
+        int count = 1;
+
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < N; j += 1) {
+                array[i][j] = new Site(count);
+                count += 1;
+            }
+        }
+
+        ufForPercolation = new WeightedQuickUnionUF(N * N + 2);
+        ufForFullness = new WeightedQuickUnionUF(N * N + 2);
+
+        for (int i = 1; i <= N; i += 1) {
+            ufForPercolation.union(virtualHead, i);
+            ufForPercolation.union(virtualTail, virtualTail - i);
+            ufForFullness.union(virtualHead, i);
+        }
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (array[row][col].isOpen) {
+            return;
+        }
+        array[row][col].isOpen = true;
+        openSize += 1;
+        if (row - 1 >= 0 && isOpen(row - 1, col)) {
+            ufForPercolation.union(indexChange(row - 1, col), indexChange(row, col));
+            ufForFullness.union(indexChange(row - 1, col), indexChange(row, col));
+        }
+        if (row + 1 < N && isOpen(row + 1, col)) {
+            ufForPercolation.union(indexChange(row + 1, col), indexChange(row, col));
+            ufForFullness.union(indexChange(row + 1, col), indexChange(row, col));
+        }
+        if (col - 1 >= 0 && isOpen(row, col - 1)) {
+            ufForPercolation.union(indexChange(row, col - 1), indexChange(row, col));
+            ufForFullness.union(indexChange(row, col - 1), indexChange(row, col));
+        }
+        if (col + 1 < N && isOpen(row, col + 1)) {
+            ufForPercolation.union(indexChange(row, col + 1), indexChange(row, col));
+            ufForFullness.union(indexChange(row, col + 1), indexChange(row, col));
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        return array[row][col].isOpen;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        return ufForFullness.connected(virtualHead, indexChange(row, col)) && isOpen(row, col);
+    }
+
+    private int indexChange(int row, int col) {
+        return row * N + col + 1;
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return openSize;
+    }
+
+
+    // does the system percolate?
+    public boolean percolates() {
+        return ufForPercolation.connected(virtualHead, virtualTail);
+    }
+
+    // use for unit testing (not required)
+    public static void main(String[] args) {
+    }
 }
+
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,90 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.Stopwatch;
 
+
+
+public class PercolationStats {
+    private PercolationFactory pf;
+    private int times;
+    private double[] data;
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        times = T;
+        data = new double[T];
+        int sumSize = N * N;
+        for (int i = 0; i < times; i += 1) {
+            Percolation p = pf.make(N);
+            int count = 0;
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                if (!p.isOpen(row, col)) {
+                    p.open(row, col);
+                    count += 1;
+                }
+                if (p.numberOfOpenSites() == sumSize) {
+                    break;
+                }
+            }
+            double temp = (double) count / sumSize;
+            data[i] = temp;
+        }
+    }
+
+    // sample mean of percolation threshold
+    //计算渗透阈值的样本平均值。
+    public double mean() {
+        return StdStats.mean(data);
+    }
+
+    // sample standard deviation of percolation threshold
+    //计算渗透阈值的样本标准差
+    public double stddev() {
+        return StdStats.stddev(data);
+    }
+
+    // low endpoint of 95% confidence interval
+    //计算 95% 置信区间的下界
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(times);
+    }
+
+    // high endpoint of 95% confidence interval
+    //计算 95% 置信区间的上界
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(times);
+    }
+
+    public static void main(String[] args) {
+        PercolationFactory pf = new PercolationFactory();
+        PercolationStats p = new PercolationStats(1000, 100, pf);
+        Stopwatch partTimer = new Stopwatch();
+        System.out.print("mean: " + p.mean());
+        System.out.print("  stddev:  " + p.stddev());
+        System.out.printf("  [ %f ,%f ]", p.confidenceLow(), p.confidenceHigh());
+        double time = partTimer.elapsedTime();
+        System.out.print("  time : " + time);
+        /*
+         * N = 100 T = 500
+         * mean: 0.5937205999999996
+         * stddev:  0.016298349242056143
+         *  [ 0.592292 ,0.595149 ]  time : 0.029
+         * N = 200 T = 500
+         * mean: 0.5922076999999999
+         * stddev:  0.009461141956669323
+         *  [ 0.591378 ,0.593037 ]  time : 0.024
+         * N = 400  T = 500
+         * mean: 0.5925924875000002
+         * stddev:  0.005673238899181237
+         *  [ 0.592095 ,0.593090 ]  time : 0.027
+
+         */
+    }
 }
